18032bde70267b9f98b32a84250701bcc80804fc
Rodrigo Cotting <cottingfontes@hotmail.com>
2025-03-30 04:16:53 -0300

    Implementacao da interface de disparo de e-mail

---
+++ Interfaces/Services/Account/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using modulum.Application.Requests.Mail;
+
+namespace modulum.Application.Interfaces.Services.Account
+{
+    public interface IEmailService
+    {
+        Task<string> SendEmail(MailRequest request);
+    }
+}
