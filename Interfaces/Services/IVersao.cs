2dc54861be0e0091ec93e298e5d84d75c7cae858
Rodrigo Cotting <cottingfontes@hotmail.com>
2025-04-15 23:34:22 -0300

    Implementado interface para getall versao de pacotes

---
+++ Interfaces/Services/IVersao.cs
@@ -0,0 +1,16 @@
+﻿using modulum.Application.Interfaces.Common;
+using modulum.Application.Responses.Versao;
+using modulum.Shared.Wrapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RCF.Modulum.Application.Interfaces.Services
+{
+    public interface IVersao : IService
+    {
+        Task<IResult<List<VersaoResponse>>> GetAllVersao();
+    }
+}
---
+++ Responses/Versao/VersaoResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modulum.Application.Responses.Versao
+{
+    public class VersaoResponse
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Versao { get; set; }
+    }
+}
