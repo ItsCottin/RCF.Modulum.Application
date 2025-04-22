19343b860e0da49218ba8b49a0bb3d24df45e660
Rodrigo Cotting <cottingfontes@hotmail.com>
2025-04-22 00:22:27 -0300

    Implementado validações no dynamic

---
+++ Validators/Requests/Dynamic/CreateDynamicFieldRequestValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using modulum.Application.Requests.Dynamic.Create;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modulum.Application.Validators.Requests.Dynamic
+{
+    public class CreateDynamicFieldRequestValidator : AbstractValidator<CreateDynamicFieldRequest>
+    {
+        public CreateDynamicFieldRequestValidator() 
+        {
+            RuleFor(request => request.NomeCampoTela).Must(x => !string.IsNullOrWhiteSpace(x)).WithMessage(x => "Campo 'Nome do Campo' é obrigatório");
+            RuleFor(request => request.Tipo).NotNull().WithMessage(x => "Campo 'Tipo' é obrigatório");
+            RuleFor(c => c.NomeCampoBase).Matches("^[a-zA-Z0-9_]+$").WithMessage("O nome do 'NomeCampoBase' deve conter apenas letras, números e underline."); // Impedir SQL Injection, precisa ser avaliado se é nescessario mais validações
+        }
+    }
+}
---
+++ Validators/Requests/Dynamic/CreateDynamicTableRequestValidator.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using modulum.Application.Requests.Dynamic.Create;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modulum.Application.Validators.Requests.Dynamic
+{
+    public class CreateDynamicTableRequestValidator : AbstractValidator<CreateDynamicTableRequest>
+    {
+        public CreateDynamicTableRequestValidator() 
+        {
+            RuleFor(request => request.CampoPK)
+                .Must(x => !string.IsNullOrWhiteSpace(x)).WithMessage(x => "Campo 'Campo PK' é obrigatório")
+                .Must((model, campoPK) => model.Campos != null && model.Campos.Any(c => c.NomeCampoBase == campoPK))
+                .WithMessage(x => "O Valor do campo 'Campo PK' não foi encontrado dentro do Objeto 'Campos'")
+                .Must((model, campoPK) => model.Campos != null && model.Campos.Any(c => c.NomeCampoBase == campoPK && c.IsPrimaryKey == true))
+                .WithMessage(x => "O campo informado em 'Campo PK' precisa estar marcado como chave primária nos campos adicionados.");
+            RuleFor(request => request.NomeTela)
+                .Must(x => !string.IsNullOrWhiteSpace(x)).WithMessage(x => "Campo 'Nome da Tela' é obrigatório");
+            RuleFor(x => x.Campos).Must(campos => campos != null && campos.Count(c => c.IsPrimaryKey) == 1)
+                .WithMessage("Deve haver exatamente um campo com 'IsPrimaryKey' = true.");
+            RuleFor(x => x.NomeTabela)
+                .Matches("^[a-zA-Z0-9_]+$")
+                .WithMessage("O nome da tabela deve conter apenas letras, números e underline."); // Impedir SQL Injection, precisa ser avaliado se é nescessario mais validações
+        }
+    }
+}
